b5417a8032dc2e9727443a2176e880869b6b600f
Max Zimmerman <william.m.zimmerman+github@gmail.com>
2022-01-20 09:46:28 -0600

    Added yasnippets functions to the dotnet file and changed the namespace to do filescoped

--- custom/languages/dotnet.el
+++ custom/languages/dotnet.el
@@ -8,6 +8,34 @@
 (define-key lsp-command-map (kbd "t p") 'lsp-csharp-run-test-at-point)
 (define-key lsp-command-map (kbd "t b") 'lsp-csharp-run-all-tests-in-buffer)
 
+;; === Functions ===
+(defun find-project-root ()
+  "Find the root of the current project directory."
+  (interactive)
+  (if (ignore-errors (eproject-root))
+      (eproject-root)
+    (or (find-git-repo (buffer-file-name)) (file-name-directory (buffer-file-name)))))
+
+(defun find-git-repo (dir)
+  "Find the git repository for DIR."
+  (if (string= "/" dir) full
+      nil
+    (if (file-exists-p (expand-file-name "../.git/" dir))
+        dir
+      (find-git-repo (expand-file-name "../" dir)))))
+
+
+(defun file-path-to-namespace ()
+  "Get the file path to the namespace."
+  (interactive)
+  (let (
+        (root (find-project-root))
+        (base (file-name-nondirectory buffer-file-name))
+        )
+    (substring (replace-regexp-in-string "/" "\." (substring buffer-file-name (length root) (* -1 (length base))) t t) 0 -1)
+    )
+  )
+
 (provide 'dotnet)
 
 ;;; dotnet.el ends here
