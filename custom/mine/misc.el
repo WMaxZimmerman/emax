7e0a7e90d13b7f19844e7ed9e9f12a5bede414b7
Max Zimmerman <william.m.zimmerman+github@gmail.com>
2022-01-26 10:50:14 -0600

    refactored to only mapconcat

--- custom/mine/misc.el
+++ custom/mine/misc.el
@@ -11,15 +11,14 @@
   (setq text (buffer-substring (region-beginning) (region-end)))
   (setq chars (mapcar 'string text))
   (setq i 0)
-  (setq new-chars (mapcar
+  (setq new-text (mapconcat
                    (lambda (char)
                      (progn (setq i (+ i 1))
                             (if (eq (% i 2) 0)
                                 (upcase char)
                               (downcase char))
                             ))
-                   chars))
-  (setq new-text (mapconcat (lambda (c) c) new-chars ""))
+                   chars ""))
 
   (kill-region (region-beginning) (region-end))
   (insert new-text))
