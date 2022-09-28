cdc8152a78388c38f8143abaa073850f6a4c0a89
Max Zimmerman <william.m.zimmerman+github@gmail.com>
2022-09-28 08:05:24 -0500

    Added search forward shortcut

--- custom/keyboard/shortcuts.el
+++ custom/keyboard/shortcuts.el
@@ -1,4 +1,5 @@
 (global-set-key (kbd "C-s") 'swiper)
+(global-set-key (kbd "M-s") 'search-forward)
 (global-set-key (kbd "M-x") 'counsel-M-x)
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
 (global-set-key (kbd "<f1> f") 'counsel-describe-function)
